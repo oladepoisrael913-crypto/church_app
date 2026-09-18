b857e247dd6686d704dc5d6d60090cf1ca046ee9
Oladepo Israel <oladepoisrael913@gmail.com>
2026-09-18 15:13:57 +0100

    remove env

--- Backend/Controller/userController.js
+++ Backend/Controller/userController.js
@@ -1,9 +1,12 @@
-import mongoose from "mongoose";
+const User = require("../models/userModel");
 import bcrypt from "bcryptjs";
 export const createUser = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = new User({ email, password });
+    const genSalt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, genSalt);
+    const user = await UserModel.create({ email, password: hashedPassword });
     await user.save();
     res.status(201).json(user);
   } catch (error) {
